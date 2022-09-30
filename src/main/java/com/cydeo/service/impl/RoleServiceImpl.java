793c4e296a9d232269d42e82e6da7eccc2838137
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2022-09-30 08:59:03 -0400

    Ticketing App MVC

--- src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -10,6 +10,8 @@ public class RoleServiceImpl extends AbstractMapService<RoleDTO,Long> implements
     @Override
     public RoleDTO save(RoleDTO role) {
         return super.save(role.getId(),role);
+
+
     }
 
     @Override
